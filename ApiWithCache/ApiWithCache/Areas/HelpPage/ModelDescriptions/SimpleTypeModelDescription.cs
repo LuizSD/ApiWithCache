1ead9fce7c81eb80edc4c8424d03e8d10d0e29d0
LuizSD <lsantosdutra@gmail.com>
2017-05-25 20:25:47 -0300

    Making a web api using a simples cache

---
+++ ApiWithCache/ApiWithCache/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ApiWithCache.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ApiWithCache/ApiWithCache/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading;
+using WebApi.OutputCache.V2;
+
+namespace ApiWithCache.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [CacheOutput(ServerTimeSpan = 120)]
+        public IEnumerable<string> Get()
+        {
+            Thread.Sleep(20000);
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
